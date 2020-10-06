10320d73c141c0ec69bfc371e81f5863a9029a5d
Uzair Mukadam <uzair.mukadam1999@gmail.com>
2020-10-06 22:59:28 +0530

    New dashboard with favourite support.

--- app/src/main/java/com/uzitech/uhome/Adapters/DatabaseAdapter.java
+++ app/src/main/java/com/uzitech/uhome/Adapters/DatabaseAdapter.java
@@ -1,21 +1,70 @@
 package com.uzitech.uhome.Adapters;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
-class DatabaseAdapter extends SQLiteOpenHelper {
-    public DatabaseAdapter(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class DatabaseAdapter extends SQLiteOpenHelper {
+
+    private final String TAG = "Database";
+
+    private String table = "favourite_tiles";
+
+    public DatabaseAdapter(Context context) {
+        super(context, "uTV_Home.db", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        sqLiteDatabase.execSQL("create table " + table + "(ID NAME UNIQUE, ICON TEXT, PKG TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("drop table if exists " + table);
+        onCreate(sqLiteDatabase);
+    }
 
+    public void addFav(String name, String pkg, String icon) {
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("ID", name);
+        contentValues.put("PKG", pkg);
+        contentValues.put("ICON", icon);
+        database.insert(table, null, contentValues);
+        Log.d(TAG, "App added");
+    }
+
+    public void removeFav(String pkg) {
+        SQLiteDatabase database = this.getWritableDatabase();
+        database.delete(table, "PKG = \"" + pkg + "\"", null);
+        Log.d(TAG, "App removed");
+    }
+
+    public ArrayList<JSONObject> getAllFav() {
+        ArrayList<JSONObject> temp = new ArrayList<>();
+        SQLiteDatabase database = this.getReadableDatabase();
+        Cursor res = database.rawQuery("select * from " + table, null);
+
+        while (res.moveToNext()) {
+            try {
+                JSONObject object = new JSONObject();
+                object.put("name", res.getString(0));
+                object.put("icon", res.getString(1));
+                object.put("pkg_name", res.getString(2));
+                temp.add(object);
+            } catch (Exception e) {
+                Log.d(TAG, e.toString());
+            }
+        }
+        res.close();
+        return temp;
     }
 }
--- app/src/main/java/com/uzitech/uhome/Dashboard.java
+++ app/src/main/java/com/uzitech/uhome/Dashboard.java
@@ -7,6 +7,7 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.CompositePageTransformer;
